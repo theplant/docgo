346f6ad51c8aed9cf567e828a8fc2f19a0388c15
Felix Sun <sunfmin@gmail.com>
2021-11-06 11:08:40 +0800

    Change to use tailwindcss

--- docjs/src/main.js
+++ docjs/src/main.js
@@ -1,9 +1,5 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import './markdown.css'
-import './layout.css'
-import './aside.css'
-import './icon.css'
 
 createApp(App).mount('#app')
