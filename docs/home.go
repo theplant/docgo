aaf99583b32c39d02e09c3cc402378e10cacce6d
Felix Sun <sunfmin@gmail.com>
2021-11-08 20:36:17 +0800

    Write some real doc for docgo

--- docs/home.go
+++ docs/home.go
@@ -5,23 +5,14 @@ import . "github.com/theplant/docgo"
 var Home = Doc(
 
 	Markdown(`
-## Overview
+## Why use docgo
 
-A common characteristic of a well-crafted API is that it’s easy to read and practically self-documenting. However, an API alone can’t convey important information like clear documentation does, such as:
+The developer community often use markdown or in Python world use [Sphinx](https://www.sphinx-doc.org) and [reStructuredText](https://docutils.sourceforge.io/rst.html), We created this to solve these problems:
 
-- The overall architecture of a framework or package
-- Relationships and dependencies between components in the API
-- Boundary conditions, side effects, and errors that occur when using the API
-
+- Work together with [snippetgo](https://github.com/sunfmin/snippetgo) to use real executable code as examples in documentation, So that it won't be invalid or obsolete after code change, Since Go compiler will pick out the errors and you will have to fix the examples to make compiler happy, and the documentation is automatically updated.
+- Write documentation in Go code not only you can still write markdown, But also you can access the flexibility of a programming language, to use components, and reuse parts that are duplicated.
 
 `),
-	Program("123").
-		Language("swift"),
-	Note("You must use the symbol’s absolute path for the page title of an extension file and include the name of the framework or package. DocC doesn’t support relative symbol paths in this context.").
-		Title("Important"),
-
-	Tip("Use a symbol’s full path to include it from elsewhere in the documentation hierarchy.").
-		Title("Tip"),
 ).
 	Tables(
 
@@ -50,7 +41,9 @@ var article1 = Doc().Tables(
 			article11,
 		),
 	),
-).Title("Doc 1").AbstractText("this is an sample abstract for doc 1")
+).Title("Doc 1").
+	AbstractText("this is an sample abstract for doc 1")
+
 var article2 = Doc().Title("Doc 2").AbstractText("this is an sample abstract for doc 2")
 var article3 = Doc().Title("Doc 3").AbstractText("this is an sample abstract for doc 3")
 var article11 = Doc().Title("Doc 1.1").AbstractText("this is an sample abstract for doc 1.1")
